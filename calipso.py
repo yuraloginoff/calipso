bf4f1e525c5729e50e79564ee7f61254f09851dc
Loginov Yura <yuraloginoff@gmail.com>
2020-02-06 23:33:01 +0300

    default font

--- calipso.py
+++ calipso.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from tkinter import *
 from tkinter import ttk
+# import tkinter.tkFileDialog
+from tkinter import font
+
 
 # create blank window
 root = Tk()
@@ -19,17 +22,23 @@ mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 
+# Fonts
+txtFont = font.Font(family='Avenir Next', size='20')
+
 # create text widget
-txt = Text(mainframe, wrap="word", padx=10, pady=10)
+txt = Text(mainframe, wrap="word", padx=10, pady=6, font=txtFont)
 txt.grid(column=0, row=0, sticky="nsew")
 txt.focus()  # autofocus
+
+
+txt.insert('1.0', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+
 
 # create scrollbar
 scrollbar = ttk.Scrollbar(root, orient=VERTICAL, command=txt.yview)
 scrollbar.grid(column=1, row=0, sticky=(N, S))
 # txt to communicate back to the scrollbar
 txt['yscrollcommand'] = scrollbar.set
-
 
 # create menus
 root.option_add('*tearOff', FALSE)
@@ -46,9 +55,16 @@ def newFile():
     print('new file')
 
 
+def save_as():
+    print('new file')
+
+
 menu_file.add_command(label='New', command=newFile)
-# menu_file.add_command(label='Open...', command=openFile)
+menu_file.add_command(label='Save as', command=save_as)
 # menu_file.add_command(label='Close', command=closeFile)
 
 
 root.mainloop()
+
+# Retrieving the Text
+# thetext = txt.get('1.0', 'end')
