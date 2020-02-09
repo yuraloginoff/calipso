72db410ef57bb9928c1517b48ea778fd436e2e66
Loginov Yura <yuraloginoff@gmail.com>
2020-02-09 17:51:05 +0300

    Update calipso.py

--- calipso.py
+++ calipso.py
@@ -21,7 +21,7 @@ root = Tk()
 root.title("Calipso")
 
 # setting window size
-root.geometry('640x200')
+root.geometry('640x480')
 root.update()
 
 mainframe = Frame(root)
@@ -55,7 +55,7 @@ txt['foreground'] = gray2
 txt['insertbackground'] = gray2  # text cursor
 txt['highlightthickness'] = 0  # no widget border
 
-txt.insert('1.0', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+txt.insert('1.0', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
 
 # create scrollbar
 scrollbar = Scrollbar(root, orient=VERTICAL, command=txt.yview)
